2a91cbf96bb22260e7f96bcf828bd3bf18e0bd2c
postelion <opr44@wp.pl>
2022-05-22 20:31:11 +0200

    update

---
+++ src/config/cv.js
@@ -0,0 +1,84 @@
+export default {
+
+    name:"DOMINIK",
+    surname:'KORDYLEWICZ',
+    school:
+    [
+        {
+            name:"Zespół Szkół Technicznych nr 1 w Pile",
+            as:"Technik Informatyk",
+            date:"2017 - 2021"
+        }
+    ],
+    experience:
+    [
+        {
+            name:"Pomocnik nauczyciela w szkole programowania",
+            description:"Pomocnik nauczyciela w szkole \
+            programowania dla młodzieży i dzieci ze szkół \
+            podstawowych i ponadpodstawowych: \
+            “Akademia Programowania Level UP”.",
+            date:"2020 - 2020"
+        },
+        {
+            name:"Pracownik fizyczny Hjort Knudsen Polen Sp. z o.o.",
+            description:"Pracownik fizyczny w jednej z firm \
+            produkującej meble.",
+            date:"2018 - 2019"
+        }
+    ],
+    about:"Były uczeń na kierunku Technik Informatyk. \
+        Posiadam znajomość takich języków \
+        programowania jak: C#, C++, JavaScript, HTML, \
+        CSS, Python. Jestem zaznajomiony z systemami \
+        operacyjnymi z rodziny Linux. Aktualnie \
+        zdobywam doświadzenie na temat systemów \
+        wbudowanych. Lubię prace w zespole, umiem \
+        myśleć logicznie podczas wykonywania swoich \
+        obowiązków. Jestem także perfekcjonistą, więc \
+        staram się wykonywać powierzone mi zadanie jak \
+        najlepiej.",
+
+    contact:
+    [
+      "Dominik@postelion.pl","Poznań 60-573 Zofii Nałkowskiej 49/16","+48 733 912 231"
+    ],
+    skills:
+    [
+        {
+            name:"C#",
+            level:8,
+        },
+        {
+            name:"JAVASCRIPT",
+            level:8,
+        },
+        {
+            name:"HTML",
+            level:8,
+        },
+        {
+            name:"CSS",
+            level:8,
+        },
+        {
+            name:"C++",
+            level:6,
+        },
+        {
+            name:"PHP",
+            level:7,
+        }
+    ],
+    languages:
+    [
+        {
+            name:"Angielski",
+            level:8
+        },
+        {
+            name:"Niemiecki",
+            level:3
+        }
+    ]
+}
