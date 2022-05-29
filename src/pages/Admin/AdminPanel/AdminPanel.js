61fe271da1df2ba621113ca4fd485bce6204defc
postelion <opr44@wp.pl>
2022-05-29 12:20:02 +0200

    update

--- src/pages/Admin/AdminPanel/AdminPanel.js
+++ src/pages/Admin/AdminPanel/AdminPanel.js
@@ -1,6 +1,7 @@
 import {ApiComponent} from '../../../lib/Classes';
 import LibraryApi from '../../../lib/Api';
 import {useEffect, useRef} from 'react';
+import Theme from '../../../lib/Theme';
 import Loading from '../../../components/Loading/Loading';
 import NoAuth from '../../../components/NoAuth/NoAuth';
 import './AdminPanel.css';
@@ -26,10 +27,10 @@ class AdminPanel extends  ApiComponent
     Success(data)
     {
         return (<div id='AdminPanel'>
-
+                <div className='Content_panel'>
                     <PanelSide select={(setting)=>{ this.SelectSetting(setting) }}/>
                     <div className='Content'><GetSetting setting={this.Setting} data={data}/></div>
-
+                </div>
         </div>)
     }
     NoAuth()
@@ -97,12 +98,34 @@ function GetSetting(props)
 }
 function ConfigGet(props)
 {
+    const inputRef=useRef();
+    const selectRef=useRef();
+    const nameRef=useRef();
+    const RemoveIcon = Theme.GetIcon('AiFillDelete');
+    function RemovePosition()
+    {
+
+    }
     function addNewPosition()
     {
-    
+        console.log(nameRef.current.value+': '+ inputRef.current.value);
+    }
+    function changeTypeInput(e)
+    {
+        if (e.target.value=='Liczba')
+        {
+            inputRef.current.type ='number';
+        }
+        else if (e.target.value=='Data')
+        {
+            inputRef.current.type ='date';
+        }
+        else if(e.target.value=='Tekst')
+        {
+            inputRef.current.type ='text';
+        }
     }
 
-    console.log(props.data[0].response[0]);
     return (<div className='Config'>
         <span>Config</span>
         <div className='data'> 
@@ -112,21 +135,29 @@ function ConfigGet(props)
                 {
                     return(
 
-                        <div key={id} name={'id['+id+']'} className='option'>{element.name+': '}<input type='date' defaultValue={element.value_date}/></div>
+                        <div key={id} name={'id['+id+']'} className='option'>{element.name+': '}<input type='date' defaultValue={element.value_date}/><RemoveIcon size='25px'/></div>
                     )
                 } else if (element.value_int != null)
                 {
-                    return(<div key={id} name={'id['+id+']'} className='option'>{element.name+': '}<input type='number' defaultValue={element.value_int}/></div>) 
+                    return(<div key={id} name={'id['+id+']'} className='option'>{element.name+': '}<input type='number' defaultValue={element.value_int}/><RemoveIcon size='25px'/></div>) 
                 } else if (element.value_string != null)
                 {
-                    return(<div key={id} name={'id['+id+']'} className='option'>{element.name+': '}<input type='text' defaultValue={element.value_string}/></div>) 
+                    return(<div key={id} name={'id['+id+']'} className='option'>{element.name+': '}<input type='text' defaultValue={element.value_string}/><RemoveIcon size='25px'/></div>) 
                 }
                 }
             )
                        
-        }
-        <div className='addNewPosition' ><div onClick={()=>{addNewPosition()}}>Dodaj Nową pozycje</div></div>
-        
+        }        
+    </div>
+    <div className='addNewPosition' >
+        <input ref={nameRef} placeholder='Nazwa' type='text'/>
+        <input ref={inputRef} placeholder='Wartość' type='text'/>
+        <select ref={selectRef} onChange={changeTypeInput}>
+            <option value='Tekst'>Tekst</option>
+            <option value='Liczba'>Liczba</option>
+            <option value='Data'>Data</option>
+        </select>
+        <div onClick={()=>{addNewPosition()}}>Dodaj Nowe Ustawienie</div>
     </div>
 </div>)
 }
