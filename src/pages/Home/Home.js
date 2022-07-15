392aa09b50005ddc9daa748bc593341f98359ba7
Postelion <opr44@wp.pl>
2022-07-15 22:59:04 +0200

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
 import './Home.css'
+import libraryTheme from '../../lib/Theme';
 
 class Home extends React.Component
 {
@@ -8,11 +9,42 @@ class Home extends React.Component
     {
         return (
             <div id="Home">
-               
+               <ChangeName value="asd"/>          
             </div>
         )
     }
 
+}
+function ChangeName(props)
+{
+    const [Status,SetStatus] = useState('text');
+    const Icon = libraryTheme.GetIcon("MdEdit");
+    const InputRef = useRef();
+
+    const save = () =>
+    {
+
+    }
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            SetStatus('text');
+        }
+    }
+
+    if(Status=='input')
+    {
+        return(<div className='NameCompany'>
+            <input ref={InputRef} autoFocus defaultValue={props.value} onKeyDown={handleKeyPress} onBlur={()=>{SetStatus('text')}}/>
+            
+            </div>)
+    }
+    else 
+    {
+        return(<div className='NameCompany' onClick={()=>{SetStatus('input')}}>
+            <div>{props.value}</div>
+            <Icon className="IconEdit" size="20px"/>
+            </div>)
+    }
 }
 
 export default Home;
